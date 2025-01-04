fc2dedc6cea3ee9d578237f0c014cf81adb99333
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-01-04 10:28:17 +0100

    corriger EX3 de tdm8 et ajouter des exercices supplementaires

---
+++ Exercices Supplémentaires - Shell/PrimeMersenne.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#Mohamed Talhaoui
+prime() {
+    P=0
+    for (( i=1; i <= $1; i++ )); do
+        if [ $(( $1 % $i )) -eq 0 ]; then
+            P=$((P + 1))
+        fi
+    done
+
+    if [ $P -eq 2 ]; then
+        echo 1
+    else
+        echo 0
+    fi
+}
+
+primeMersenne() {
+    for (( i=1; i < $1; i++ )); do
+        if [ $(prime $i) -eq 1 ]; then
+            if [ $(( 2**$i - 1 )) -eq $1 ]; then # 2ⁿ - 1 == q | n : le nombre premier, q : le nombre va saisi par utilisateur
+                echo "$1 est un Premier de Mersenne"
+                exit # une fois ikhraj lina bli premier nkhrjo mn program
+            fi
+        fi
+    done
+    echo "$1 n'est pas Premier de Mersenne"
+}
+
+read -p "Entrer un nombre impair : " q
+
+if (( q % 2 == 0 )); then
+    echo "Veuillez entrer un nombre impair !"
+else
+    primeMersenne $q
+fi
+
+
+# Exemple :
+# q = 7 >>>>> on va verifie tous les 2ⁿ - 1 qui egale 7
+# 7 est un Premier de Mersenne >>>>> 2³ - 1 == 7 | avec 3 est un nombre premier
--- TDM8/EX3.bash
+++ TDM8/EX3.bash
@@ -4,10 +4,17 @@ gedit premier.sh
 #!/bin/bash
 
 premier(){
-    if [ `expr $1 % 2` -ne 0 ] || [ $1 -eq 2  ];then
-        echo "premier"
+    P=0
+    for (( i=1; i <= $1; i++ )); do
+        if [ $(( $1 % $i )) -eq 0 ]; then
+            P=$((P + 1))
+        fi
+    done
+
+    if [ $P -eq 2 ]; then
+        echo "$1 est un Nombre Premier"
     else
-        echo "non premier"
+        echo "$1 n'est pas un Nombre Premier"
     fi
 }
 premier $1
