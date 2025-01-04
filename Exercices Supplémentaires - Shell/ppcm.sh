f73dbe36b27e86732226e287a256062d2a52458c
Mohamed Talhaoui <144726758+mohamedtalhaouii@users.noreply.github.com>
2025-01-04 10:54:53 +0100

    ajouter ex de ppcm

---
+++ Exercices Supplémentaires - Shell/ppcm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Mohamed Talhaoui
+
+pgcd(){
+    if [ $2 -eq 0 ]; then
+        echo $1
+    else
+        pgcd $2 `expr $1 % $2`
+    fi
+}
+ppcm(){
+    echo $(( ($1 * $2) / $(pgcd $1 $2) ))
+}
+
+read -p "Veuillez Saisir deux Entiers Positives : " a b
